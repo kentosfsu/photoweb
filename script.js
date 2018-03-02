0ba8a76055c686d087ed7a8c8a9bb72e786a41be
kentosfsu <32149707+kentosfsu@users.noreply.github.com>
2018-03-02 14:41:59 -0800

    implemented menubar hovaring with js

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 $(function() {
+  // $('#submenu').fadeIn(1500);
+
+  $('#photo_menu').hover(
+    function() {
+      $('#submenu').fadeIn();
+    },
+    function() {
+      $('#submenu').fadeOut();
+    }
+  );
+
 });
